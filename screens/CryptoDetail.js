1b2a2c7c6e7ffad2481daff2b695b3339698de89
Yijian <yijian.chen@dzhintl.com>
2022-04-27 22:32:41 +0800

    CrytoDetailPage

--- screens/CryptoDetail.js
+++ screens/CryptoDetail.js
@@ -1,41 +1,103 @@
-import React from 'react';
+import react from "react";
+import React from "react";
 import {
-    StyleSheet,
-    View,
-    Text,
-    TouchableOpacity
-} from 'react-native';
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Animated,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { HeaderBar, CurrencyLabel } from "../components";
 
-const CryptoDetail = ({ navigation }) => {
+import { dummyData, SIZES, FONTS, icons, COLORS } from "../constants";
+
+const CryptoDetail = ({ route, navigation }) => {
+  const [selectedCurrency, setSelectedCurrency] = React.useState(null);
+
+  React.useEffect(() => {
+    const { currency } = route.params;
+    setSelectedCurrency(currency);
+  }, []);
+
+  function renderChart() {
     return (
-        <View style={styles.container}>
-            <Text>CryptoDetail</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Transaction")}
+      <View
+        style={{
+          marginTop: SIZES.padding,
+          marginHorizontal: SIZES.radius,
+          alignItems: "center",
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.white,
+          ...styles.shadow,
+        }}
+      >
+        {/* Header */}
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: SIZES.padding,
+            paddingHorizontal: SIZES.padding,
+          }}
+        >
+          <View style={{ flex: 1 }}>
+            <CurrencyLabel
+              icon={selectedCurrency?.image}
+              currency={selectedCurrency?.currency}
+              code={selectedCurrency?.code}
+            />
+          </View>
+          <View>
+            <Text style={{ ...FONTS.h3 }}>${selectedCurrency?.amount}</Text>
+            <Text
+              style={{
+                color:
+                  selectedCurrency?.type == "I" ? COLORS.green : COLORS.red,
+                ...FONTS.body3,
+              }}
             >
-                <Text>Navigate to Transaction</Text>
-            </TouchableOpacity>
+              {selectedCurrency?.changes}
+            </Text>
+          </View>
         </View>
-    )
-}
+        {/* Chart */}
+        {/* Options */}
+        {/* Dots */}
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightGray1 }}>
+      <HeaderBar right={true} />
+      <ScrollView>
+        <View style={{ flex: 1, paddingBottom: SIZES.padding }}>
+          {renderChart()}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    shadow: {
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.30,
-        shadowRadius: 4.65,
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
 
-        elevation: 8,
-    }
-})
+    elevation: 8,
+  },
+});
 
 export default CryptoDetail;
